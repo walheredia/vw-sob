1ebd1fd53ece778e43f75f8a9924d84ba480e3f8
Walter Heredia <waldarioh@gmail.com>
2023-06-13 15:48:25 -0300

    adding external auth module

---
+++ vwsob/AuthClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Configuration;
+
+public class AuthClient
+{
+    private readonly string _endpointUrl = ConfigurationManager.AppSettings["EndpointUrl"];
+    private readonly string _clientId = ConfigurationManager.AppSettings["ClientId"];
+    private readonly string _clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+
+    public AuthClient()
+    {
+        
+    }
+
+    public async Task<string> GetAccessToken()
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var requestContent = new StringContent(
+                "grant_type=client_credentials" +
+                "&client_id=" + Uri.EscapeDataString(_clientId) +
+                "&client_secret=" + Uri.EscapeDataString(_clientSecret),
+                Encoding.UTF8,
+                "application/x-www-form-urlencoded");
+
+            var response = await httpClient.PostAsync(_endpointUrl, requestContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(responseBody);
+                return tokenResponse.access_token;
+            }
+            else
+            {
+                throw new Exception("Error en la solicitud de autenticación. Código de respuesta: " + response.StatusCode);
+            }
+        }
+    }
+}
+
+// Clase para representar la estructura del token JSON
+public class TokenResponse
+{
+    public string access_token { get; set; }
+    public string token_type { get; set; }
+    public int expires_in { get; set; }
+    public string user_id { get; set; }
+}
---
+++ vwsob/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Crea una instancia del cliente de autenticación
+        var authClient = new AuthClient();
+
+        try
+        {
+            string accessToken = authClient.GetAccessToken().GetAwaiter().GetResult();
+            Console.WriteLine(accessToken);
+
+            // Continúa con las operaciones adicionales utilizando el access token obtenido
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadKey();
+        }
+    }
+}
